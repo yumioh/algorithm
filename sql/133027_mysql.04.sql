b764063bb9f0aa0d194f2b4ac4ae41bdd75d78de
yumi oh <13yooms@gmail.com>
2023-12-14 15:50:38 +0900

    join 연습

---
+++ sql/133027_mysql.04.sql
@@ -0,0 +1,11 @@
+--7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개의 맛을 조회하는 SQL 문을 작성해주세요.
+
+SELECT FLAVOR
+FROM 
+(
+    SELECT A.FLAVOR, (SUM(A.TOTAL_ORDER) + SUM(B.TOTAL_ORDER)) AS TOTAL
+    FROM FIRST_HALF A, JULY B
+    WHERE A.FLAVOR = B.FLAVOR
+    GROUP BY A.FLAVOR
+    ORDER BY TOTAL DESC
+) AS TOTAL_TB LIMIT 3
